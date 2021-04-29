1da99d4f2af68edcc72f057e3d788b61cf93cc17
Jackson Moody <jcksnmdy1@gmail.com>
2021-04-28 22:18:19 -0400

    for arduino

--- playMusicRed.py
+++ playMusicRed.py
@@ -3,36 +3,20 @@ import os
 import time
 from broadcastDisplay import toColor
 import paho.mqtt.client as mqtt
-import pyfirmata
 import sys
 sys.path.append('/Users/s1034274/Desktop/globals/')
 from constants import path
+#!/usr/bin/env python3
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser.flush()
 
 MQTT_SERVER = "192.168.1.119"
 flag = "red"
 
 MQTT_PATH = "test_channel"
-
-board = pyfirmata.Arduino('/dev/ttyACM0')
-redLeft = board.get_pin('d:2:o')
-greenLeft = board.get_pin('d:3:o')
-blueLeft = board.get_pin('d:4:o')
-redMid = board.get_pin('d:5:o')
-greenMid = board.get_pin('d:6:o')
-blueMid = board.get_pin('d:7:o')
-redRight = board.get_pin('d:8:o')
-greenRight = board.get_pin('d:9:o')
-blueRight = board.get_pin('d:10:o')
-
-redLeft.write(0)
-greenLeft.write(0)
-blueLeft.write(0)
-redMid.write(0)
-greenMid.write(0)
-blueMid.write(0)
-redRight.write(0)
-greenRight.write(0)
-blueRight.write(0)
 
 
 def play(num):
@@ -40,24 +24,12 @@ def play(num):
     i = 5
     df = pd.read_excel(path + "/flagCode/song" + str(num) + "/" + flag + ".xlsx")
     while (i < len(df)):
-        if(toColor(df.loc[(i),'red Left'])==(255,0,0)):
-            print("Red")
-            redLeft.write(1)
-            greenLeft.write(0)
-            blueLeft.write(0)
-        elif(toColor(df.loc[(i),'red Left'])==(255,255,255)):
-            print("White")
-            redLeft.write(1)
-            greenLeft.write(1)
-            blueLeft.write(1)
-        else:
-            print("Off")
-            redLeft.write(0)
-            greenLeft.write(0)
-            blueLeft.write(0)
-        print(str(i) + " " + str(df.loc[(i),'Red Flag Left']))
-        time.sleep(0.03)
-        i+=1
+        ser.write(b"" + toColor(df.loc[(i),'red Left']) + "\n")
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        time.sleep(0.01)
+        print(str(i) + " " + str(df.loc[(i),'red Left']))
+        i+=3
     print("Done")
  
 # The callback for when the client receives a CONNACK response from the server.
