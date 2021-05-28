844b9982147a2df44789fd902450b2dec2d4066b
jcksnmdy <jcksnmdy1@gmail.com>
2021-05-28 19:48:14 -0400

    installs

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y mosquitto mosquitto-clients
+sudo apt-get install libatlas-base-dev
+pip3 install openpyxl
+pip3 install paho-mqtt
+pip3 install pandas
+git config --global user.email "jcksnmdy1@gmail.com"
+git config --global user.name "jcksnmdy"
