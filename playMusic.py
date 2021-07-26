7b4bfe4384e5d0c0a98f3138278b229827d48258
Jackson Moody <jcksnmdy1@gmail.com>
2021-07-26 19:23:56 -0400

    New songs

--- playMusic.py
+++ playMusic.py
@@ -297,14 +297,14 @@ def on_message(client, userdata, msg):
         if ("HIT" in update()):
             os.system('mosquitto_pub -h ' + MQTT_SERVER + ' -t test_channel -m "hit"')
     if("song" in str(msg.payload)):
-    	Rmsg = str(msg.payload)
-		print("Song" + str(Rmsg[4]))
-		play(int(Rmsg[4]))
+        Rmsg = str(msg.payload)
+        print("Song" + str(Rmsg[4]))
+        play(int(Rmsg[4]))
 
     if("load" in str(msg.payload)):
-    	Rmsg = str(msg.payload)
-		print("Loading"  + str(Rmsg[4]))
-		loadSong(int(Rmsg[4]))
+        Rmsg = str(msg.payload)
+        print("Loading"  + str(Rmsg[4]))
+        loadSong(int(Rmsg[4]))
 
     if("wait" in str(msg.payload)):
         print("Waiting to be hit")
