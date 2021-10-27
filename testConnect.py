7361a5d94ad4e2c0c4aef2c32750520393fc2f0c
jcksnmdy <jcksnmdy1@gmail.com>
2021-10-26 20:39:42 -0400

    gg

--- testConnect.py
+++ testConnect.py
@@ -1,14 +1,25 @@
 import time
 import urllib.request
 
+import serial
+
+ser = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
+ser.flush()
+ser.write(b"" + "(255.0, 255.0, 255.0)(255.0, 255.0, 0.0)(255.0, 255.0, 0.0)".encode('ascii') + "\n".encode('ascii'))
+
+
+
+
 print("Pausing for 15 seconds")
 time.sleep(15)
 
 def connect(host='http://google.com'):
     try:
         urllib.request.urlopen(host) #Python 3.x
+        ser.write(b"" + str(df.loc[(5),flag + ' Left']).encode('ascii') + str(df.loc[(5),flag + ' Left']).encode('ascii') + "(0.0, 0.0, 0.0)".encode('ascii') + "\n".encode('ascii'))
         return True
     except:
+        ser.write(b"" + "(255.0, 0.0, 0.0)(255.0, 0.0, 0.0)(255.0, 0.0, 0.0)".encode('ascii') + "\n".encode('ascii'))
         return False
 # test
 while not connect():
@@ -16,3 +27,4 @@ while not connect():
     print("no internet!")
 
 print("Internet Found! Getting new code")
+        ser.write(b"" + str(df.loc[(5),flag + ' Left']).encode('ascii') + str(df.loc[(5),flag + ' Left']).encode('ascii') + "(0.0, 0.0, 0.0)".encode('ascii') + "\n".encode('ascii'))
