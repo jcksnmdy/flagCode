bb350b4c049f7e9b1951268a0f483f8db05864a1
jcksndy <jcksnmdy1@gmail.com>
2022-02-21 20:49:41 -0500

    songs

--- playMusic.py
+++ playMusic.py
@@ -367,17 +367,17 @@ def on_message(client, userdata, msg):
             os.system('mosquitto_pub -h ' + MQTT_SERVER + ' -t test_channel -m "hit"')
     if("song" in str(msg.payload)):
         Rmsg = str(msg.payload)
-        print("Song" + str(Rmsg[6:len(Rmsg)-1]]))
+        print("Song" + str(Rmsg[6:len(Rmsg)-1]))
         ser.write(b"" + "notMode".encode('ascii') + "\n".encode('ascii'))
         songStart = threading.Thread(group=None, target=play, args=(int(Rmsg[6:len(Rmsg)-1]),), name=None)
         songStart.start()
 
     if("load" in str(msg.payload)):
         Rmsg = str(msg.payload)
-        print("Loading"  + str(Rmsg[6:len(Rmsg)-1]]))   
+        print("Loading"  + str(Rmsg[6:len(Rmsg)-1]))   
         prepareLook = threading.Thread(group=None, target=prepareTurn, name=None)
         prepareLook.start()
-        loadSong(int(Rmsg[6:len(Rmsg)-1]]))
+        loadSong(int(Rmsg[6:len(Rmsg)-1]))
         if (prepareLook.is_alive()):
             prepareLook.join()
 
